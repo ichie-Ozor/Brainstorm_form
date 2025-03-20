9d94cbc78371390c9d153ef19a5d5bb6dcbdebc8
Ichie Ozor <simeon_mc2000@yahoo.com>
2025-03-20 11:00:43 +0100

    moved backend

--- backend/src/routes/form.js
+++
@@ -1,7 +0,0 @@
-const { createForm, getForm } = require("../formController")
-const upload = require('../util/multer')
-
-module.exports = (app) => {
-    app.post("/form", upload.single('file'), createForm)
-    app.get("/form", getForm)
-}
--- backend/src/uploads.js
+++
@@ -1,3 +0,0 @@
-const upload = () => { }
-
-module.exports = upload
